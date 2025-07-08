b767fcfec8d64d98464df340559c4e59a706dc4b
mohamedFathy98 <mdx7700@gmail.com>
2025-07-08 14:41:36 +0300

    -- Update view for Order & Product to match the design of Home

---
+++ OrderTask/OrderTask/ViewModel/PageFilterVM.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderTask.ViewModel
+{
+    public class PageFilterVM
+    {
+        public int pageNumber { get; set; }
+        public int pageSize { get; set; }
+        public string searchString { get; set; }
+    }
+}
